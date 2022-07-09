48a0ef60b3d39af22d810f86aa3b269a145cf6b8
vladimir <chertonoivskiy@ucloud.com>
2022-07-09 22:43:12 +0600

    task8 first commit

---
+++ task8/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Программа, на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/ 
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count < N+1) {
+    if (count%2 == 0)
+    Console.Write(count + " ");
+    count++;
+}
+Console.WriteLine(" ");
