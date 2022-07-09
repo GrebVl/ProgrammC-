a4faf0930e1964e466c0cea3d5e38044abef6d75
vladimir <chertonoivskiy@ucloud.com>
2022-07-09 21:46:21 +0600

    task6 first commit

---
+++ task6/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Программа, на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/ 
+
+Console.WriteLine("Ввведите число a:");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("a=" + firstNum);
+
+int i = firstNum % 2;
+
+if (i == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
